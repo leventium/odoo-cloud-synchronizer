58898d049135ce5ad6426c638959d2299404ce4e
Leon <limiroshnichenko@miem.hse.ru>
2023-03-31 15:46:41 +0300

    ✨ Add responses

---
+++ server/src/responses.py
@@ -0,0 +1,55 @@
+import os
+from urllib.parse import urlparse, urlencode, urlunparse
+from fastapi import Response, status
+from fastapi.responses import RedirectResponse
+
+
+YANDEX_OAUTH = "https://oauth.yandex.ru/authorize"
+
+
+def redirect_to(url: str):
+    return RedirectResponse(url, status_code=status.HTTP_303_SEE_OTHER)
+
+
+def redirect_to_yandex_oauth(state: str = ""):
+    redirect_url = list(urlparse(YANDEX_OAUTH))
+    redirect_url[4] = urlencode({
+        "response_type": "code",
+        "client_id": os.environ["YANDEX_CLIENT_TOKEN"],
+        "state": state
+    })
+    redirect_url = urlunparse(redirect_url)
+    return RedirectResponse(redirect_url, status_code=status.HTTP_303_SEE_OTHER)
+
+
+def serialize_instances(instances: dict):
+    if len(instances) == 0:
+        result = "Empty."
+    else:
+        result = ""
+        for inst in instances:
+            result += f"{inst['url']}\t\t{inst['db_name']}\n"
+    return Response(
+        status_code=status.HTTP_200_OK,
+        media_type="text/plain",
+        content=result
+    )
+
+
+INVALID_TOKEN = Response(
+    status_code=status.HTTP_401_UNAUTHORIZED,
+    media_type="text/plain",
+    content="Invalid token."
+)
+
+SUCCESS_INSERTION = Response(
+    status_code=status.HTTP_201_CREATED,
+    media_type="text/plain",
+    content="Odoo instance inserted successfully."
+)
+
+SUCCESS_DELETION = Response(
+    status_code=status.HTTP_201_CREATED,
+    media_type="text/plain",
+    content="Odoo instance deleted successfully."
+)
